fd1e283ab31f044ed789cbdde5512e2094ff5819
Steph <stephfmoore@gmail.com>
2020-02-24 21:03:04 +0000

    added more css styling

--- src/components/SingleArticlePage.jsx
+++ src/components/SingleArticlePage.jsx
@@ -1,36 +1,42 @@
 import React, { Component } from "react";
 import axios from "axios";
-import styles from "../css-styles/SingleArticlePage.module.css"
+import styles from "../css-styles/SingleArticlePage.module.css";
+import LoadingIndicator from "./LoadingIndicator";
 
 class SingleArticlePage extends Component {
   state = {
-   articleInfo: {}
+    articleInfo: {}
   };
 
   getArticleInfo = () => {
     const { article_id } = this.props;
 
-    axios.get(`https://steph-nc-news-app.herokuapp.com/api/articles/${article_id}`, {
-      params: { article_id }
-    }).then(response => {
-      this.setState({articleInfo: response.data.article})
-    });
+    axios
+      .get(
+        `https://steph-nc-news-app.herokuapp.com/api/articles/${article_id}`,
+        {
+          params: { article_id }
+        }
+      )
+      .then(response => {
+        this.setState({ articleInfo: response.data.article });
+      });
   };
 
   componentDidMount = () => {
     this.getArticleInfo();
   };
 
-  componentDidUpdate = (prevProps) => {
+  componentDidUpdate = prevProps => {
     if (prevProps.uri !== this.props.uri) {
-    this.getArticleInfo();
+      this.getArticleInfo();
     }
-  }
+  };
 
   render() {
     const { title, body, author } = this.state.articleInfo;
     
-    
+    //need to add in isLoading and the loading indicator
 
     return (
       <div className={styles.articleContainer}>
